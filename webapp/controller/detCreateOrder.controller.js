a5a14558afcaaad004bf292361ef35147bd92666
MarcPorath <marc.porath@qpcm.de>
2018-01-11 08:19:04 +0000

    Initial Release

---
+++ webapp/controller/detCreateOrder.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("EndersApp.controller.detCreateOrder", {
+
+		onInit: function() {
+
+		}
+
+		//	onExit: function() {
+		//
+		//	}
+
+	});
+
+});
