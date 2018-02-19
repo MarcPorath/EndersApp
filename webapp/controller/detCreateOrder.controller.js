a879776d449b74aeffab8c6700118027031c5a3b
MarcPorath <marc.porath@qpcm.de>
2018-02-19 11:31:57 +0000

    Zwischenstand SmartFilterTable + Annotations

--- webapp/controller/detCreateOrder.controller.js
+++ webapp/controller/detCreateOrder.controller.js
@@ -7,6 +7,7 @@ sap.ui.define([
 	return BaseController.extend("EndersApp.controller.detCreateOrder", {
 		onInit: function() {
 			this.getView().setModel(this.getOwnerComponent().getModel("basket"));
+			debugger;
 		}
 
 	// 	onInit: function() {
