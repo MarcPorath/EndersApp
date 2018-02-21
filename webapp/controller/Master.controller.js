395e999db2d82c752f0cce8e8b646571e45f05b6
PDEnd <pdvoenko@enders.de>
2018-02-21 12:19:54 +0000

    Search Dialog: Strasse und Name 2 ergänzt DebInfo: Vorläufige Testdaten eingesetzt

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -1,20 +1,31 @@
 sap.ui.define(["sap/ui/core/mvc/Controller"],
+
 	function(Controller) {
 		"use strict";
-		 var filters = [];
-		 var selectedCust = "";
+		var filters = [];
+		var selectedCust = "";
+		var selectedCheck = "";
+
 		return Controller.extend("EndersApp.controller.Master", {
 
 			onInit: function() {
 
+				this.checkSelection();
+				
+			},
+
+			checkSelection: function() {
 				if (this.byId("kunnr_m").getText() === "") {
 					this.byId("ActionListMain").setVisible(false);
 					this.byId("displayDeb").setVisible(false);
 					this.byId("labSelDeb").setVisible(true);
+					selectedCheck = false;
 				} else {
 					this.byId("ActionListMain").setVisible(true);
 					this.byId("displayDeb").setVisible(true);
 					this.byId("labSelDeb").setVisible(false);
+					selectedCheck = true;
+				
 				}
 
 			},
@@ -29,18 +40,16 @@ sap.ui.define(["sap/ui/core/mvc/Controller"],
 
 					oView.addDependent(oDialog);
 				}
-				debugger;
+
 				oDialog.open();
 
-
-
 			},
-			closeSearchDialog: function(){
+			closeSearchDialog: function() {
 				this.getView().byId("searchDebDialog").close();
 				sap.m.MessageToast.show("Es wurde kein Kunde ausgewählt");
 			},
 			toCreateOrder: function(oEvent) {
-				if (selectedCust === "" ) {
+				if (selectedCust === "") {
 					sap.m.MessageToast("Bitte vorher einen Kunden auswählen");
 				} else {
 					var oMainView = sap.ui.getCore().byId("__xmlview0");
@@ -53,7 +62,7 @@ sap.ui.define(["sap/ui/core/mvc/Controller"],
 					var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 					oRouter.navTo("detCreateOrder");
 				}
-				
+
 			},
 
 			toShowOrder: function(oEvent) {
@@ -62,30 +71,42 @@ sap.ui.define(["sap/ui/core/mvc/Controller"],
 				oRouter.navTo("showOrderSelect");
 			},
 			getCustomer: function() {
-				return this.selectedCust;	
+				return this.selectedCust;
 			},
 			navToDeb: function(oEvent) {
 				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 				oRouter.navTo("showDebInf");
 			},
-			
-			entryDeb: function(oEvent){
-				this.getView().byId("searchDebDialog").close();
+
+			entryDeb: function(oEvent) {
 				this.byId("kunnr_m").setText(selectedCust.kunnr);
-				this.byId("name1_m").setText(selectedCust.name1);
-				this.byId("name2_m").setText(selectedCust.name2);
-				this.byId("tel01_m").setText(selectedCust.tel01);
-				this.byId("ort01_m").setText(selectedCust.ort01);
-				this.byId("plz01_m").setText(selectedCust.plz01);
-				this.byId("ActionListMain").setVisible(true);
-				this.byId("displayDeb").setVisible(true);
-				this.byId("labSelDeb").setVisible(false);				
+				this.checkSelection();
+			
+				this.getView().byId("searchDebDialog").close();
+				if (selectedCheck === true) {
+					this.byId("kunnr_m").setText(selectedCust.kunnr);
+					this.byId("name1_m").setText(selectedCust.name1);
+					this.byId("name2_m").setText(selectedCust.name2);
+					this.byId("tel01_m").setText(selectedCust.tel01);
+					this.byId("ort01_m").setText(selectedCust.ort01);
+					this.byId("plz01_m").setText(selectedCust.plz01);
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+///AUSBAUEN SOBALD INFOSCREEN ANGEPASST!!!!!
+					window.selCustGlobal = selectedCust;
+///////////////////////////////////////////////////////////////////////////////////////////
+				
+				}
+				
 			},
 			onItemPress: function(oEvent) {
-				
+
 				selectedCust = oEvent.getParameter("listItem").getBindingContext().getObject();
+				debugger;
 				this.entryDeb(oEvent);
-				
+
 			},
 			selectItem: function(oControlEvent) {
 				selectedCust = oControlEvent.getParameter("listItem").getBindingContext().getObject();
@@ -93,38 +114,38 @@ sap.ui.define(["sap/ui/core/mvc/Controller"],
 			filterData: function(oEvent) {
 				this._filterData(oEvent.getParameter("newValue"), oEvent.getSource().getBindingInfo("placeholder").binding.sPath);
 			},
-			_filterData: function(str,spath){
+			_filterData: function(str, spath) {
 				var i = 0;
 				if (filters.length > 0) {
 					for (var s in filters) {
-						if (filters[s].sPath === spath ){
-							if ( str === "" ) {
-								filters.splice(s, 1);	
+						if (filters[s].sPath === spath) {
+							if (str === "") {
+								filters.splice(s, 1);
 							} else {
 								filters.splice(s, 1, new sap.ui.model.Filter(spath, sap.ui.model.FilterOperator.Contains, str));
 							}
 							i = 1;
 						}
-					}					
+					}
 				}
-				if ( i < 1 ){
+				if (i < 1) {
 					filters.push(new sap.ui.model.Filter(spath, sap.ui.model.FilterOperator.Contains, str));
 				}
 				this._applyOnModel();
 			},
 			// filter OData with Filter Array
 			_applyOnModel: function() {
-				this.getView().byId("table_deb").getBinding("items").filter([]);
-				if (filters.length > 0) {
-					this.getView().byId("table_deb").getBinding("items").filter(filters, sap.ui.model.FilterType.Application);
+					this.getView().byId("table_deb").getBinding("items").filter([]);
+					if (filters.length > 0) {
+						this.getView().byId("table_deb").getBinding("items").filter(filters, sap.ui.model.FilterType.Application);
+					}
 				}
-			}
-			// _readSpecificCustomer: function(){
-			// 	var aFilter = [];
-			// 	var oBinding;
-			// 	aFilter.push(new sap.ui.model.Filter("kunnr", sap.ui.model.FilterOperator.EQ, selectedCust));
-			// 	oBinding = this.getView().getModel().bindList("/Customer");
-			// 	oBinding.filter(aFilter);
-			// }					
+				// _readSpecificCustomer: function(){
+				// 	var aFilter = [];
+				// 	var oBinding;
+				// 	aFilter.push(new sap.ui.model.Filter("kunnr", sap.ui.model.FilterOperator.EQ, selectedCust));
+				// 	oBinding = this.getView().getModel().bindList("/Customer");
+				// 	oBinding.filter(aFilter);
+				// }					
 		});
 	});
--- webapp/controller/showDebInf.controller.js
+++ webapp/controller/showDebInf.controller.js
@@ -2,15 +2,39 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller"
 ], function(Controller) {
 	"use strict";
-
+	
 	return Controller.extend("EndersApp.controller.showDebInf", {
 		goToOP: function(){
 			// muss noch abgeklärt werden, da hier ein Verstoß gegen die Richtlinien vorliegt
 			window.open("http://saperp.enders.global:8000/sap/bc/gui/sap/its/webgui?~transaction=ZFI_OP_TOUR", "_blank");
 			}
+,
+onInit: function(){
+	//Ausbauen sobald binding erfolgt
+	debugger;
 	
 
 
+
+	this.byId("l_kunnr").setText(window.selCustGlobal.kunnr);
+		this.byId("l_name1").setText(window.selCustGlobal.name1);
+		this.byId("l_name2").setText(window.selCustGlobal.name2);
+		this.byId("l_name3").setText(window.selCustGlobal.name3);
+		this.byId("l_name4").setText(window.selCustGlobal.name4);
+		this.byId("l_stras").setText(window.selCustGlobal.stras);
+		this.byId("l_plz01").setText(window.selCustGlobal.plz01);
+		this.byId("l_ort01").setText(window.selCustGlobal.ort01);
+		this.byId("l_tel01").setText(window.selCustGlobal.tel01);
+		this.byId("l_telfx").setText(window.selCustGlobal.telfx);
+		this.byId("l_mail").setText(window.selCustGlobal.mail);
+		this.byId("l_konda").setText(window.selCustGlobal.konda);
+		this.byId("l_optotal").setText(window.selCustGlobal.optotal);
+		this.byId("l_oldop").setText(window.selCustGlobal.oldop);
+		this.byId("l_olddate").setText(window.selCustGlobal.olddate);
+		this.byId("l_note").setValue(window.selCustGlobal.note);
+	
+		
+}
 	});
 
 });
