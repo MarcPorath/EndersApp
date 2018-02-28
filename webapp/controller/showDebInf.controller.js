768799c46d43070a3511603cdbaec3118b14ec0b
PDEnd <pdvoenko@enders.de>
2018-02-28 13:32:29 +0000

    Änderung Basket

--- webapp/controller/showDebInf.controller.js
+++ webapp/controller/showDebInf.controller.js
@@ -6,34 +6,27 @@ sap.ui.define([
 	return Controller.extend("EndersApp.controller.showDebInf", {
 		goToOP: function(){
 			// muss noch abgeklärt werden, da hier ein Verstoß gegen die Richtlinien vorliegt
-			//window.open("http://saperp.enders.global:8000/sap/bc/gui/sap/its/webgui?~transaction=ZFI_OP_TOUR", "_blank");
+			window.open("http://saperp.enders.global:8000/sap/bc/gui/sap/its/webgui?~transaction=ZFI_OP_TOUR", "_blank");
 			}
 ,
 onInit: function(){
-	//Ausbauen sobald binding erfolgt
-	
-	
 	debugger;
-	
-
-
-
-//	this.byId("l_kunnr").setText(window.selCustGlobal.kunnr);
-//		this.byId("l_name1").setText(window.selCustGlobal.name1);
-//		this.byId("l_name2").setText(window.selCustGlobal.name2);
-//		this.byId("l_name3").setText(window.selCustGlobal.name3);
-//		this.byId("l_name4").setText(window.selCustGlobal.name4);
-//		this.byId("l_stras").setText(window.selCustGlobal.stras);
-//		this.byId("l_plz01").setText(window.selCustGlobal.plz01);
-//		this.byId("l_ort01").setText(window.selCustGlobal.ort01);
-//		this.byId("l_tel01").setText(window.selCustGlobal.tel01);
-//		this.byId("l_telfx").setText(window.selCustGlobal.telfx);
-//		this.byId("l_mail").setText(window.selCustGlobal.mail);
-//		this.byId("l_konda").setText(window.selCustGlobal.konda);
-//		this.byId("l_optotal").setText(window.selCustGlobal.optotal);
-//		this.byId("l_oldop").setText(window.selCustGlobal.oldop);
-//		this.byId("l_olddate").setText(window.selCustGlobal.olddate);
-//		this.byId("l_note").setValue(window.selCustGlobal.note);
+		this.byId("l_kunnr").setText(this.getOwnerComponent().selectedCust.kunnr);
+		this.byId("l_name1").setText(this.getOwnerComponent().selectedCust.name1);
+		this.byId("l_name2").setText(this.getOwnerComponent().selectedCust.name2);
+		this.byId("l_name3").setText(this.getOwnerComponent().selectedCust.name3);
+		this.byId("l_name4").setText(this.getOwnerComponent().selectedCust.name4);
+		this.byId("l_stras").setText(this.getOwnerComponent().selectedCust.stras);
+		this.byId("l_plz01").setText(this.getOwnerComponent().selectedCust.plz01);
+		this.byId("l_ort01").setText(this.getOwnerComponent().selectedCust.ort01);
+		this.byId("l_tel01").setText(this.getOwnerComponent().selectedCust.tel01);
+		this.byId("l_telfx").setText(this.getOwnerComponent().selectedCust.telfx);
+		this.byId("l_mail").setText(this.getOwnerComponent().selectedCust.mail);
+		this.byId("l_konda").setText(this.getOwnerComponent().selectedCust.konda);
+		this.byId("l_optotal").setText(this.getOwnerComponent().selectedCust.optotal);
+		this.byId("l_oldop").setText(this.getOwnerComponent().selectedCust.oldop);
+		this.byId("l_olddate").setText(this.getOwnerComponent().selectedCust.olddate);
+		this.byId("l_note").setValue(this.getOwnerComponent().selectedCust.note);
 	
 		
 }
