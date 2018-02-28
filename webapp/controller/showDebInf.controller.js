240cc4de107b838c2fe5d871ace1abbabd8aa9c9
MarcPorath <marc.porath@qpcm.de>
2018-02-28 07:51:51 +0000

    Bugfix

--- webapp/controller/showDebInf.controller.js
+++ webapp/controller/showDebInf.controller.js
@@ -6,7 +6,7 @@ sap.ui.define([
 	return Controller.extend("EndersApp.controller.showDebInf", {
 		goToOP: function(){
 			// muss noch abgeklärt werden, da hier ein Verstoß gegen die Richtlinien vorliegt
-			window.open("http://saperp.enders.global:8000/sap/bc/gui/sap/its/webgui?~transaction=ZFI_OP_TOUR", "_blank");
+			//window.open("http://saperp.enders.global:8000/sap/bc/gui/sap/its/webgui?~transaction=ZFI_OP_TOUR", "_blank");
 			}
 ,
 onInit: function(){
