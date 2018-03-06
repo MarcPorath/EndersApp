411202db939fc47f2197937378723bbec9b1b31d
MarcPorath <marc.porath@qpcm.de>
2018-03-06 08:07:18 +0000

    Backrouting

--- webapp/controller/matDetail.controller.js
+++ webapp/controller/matDetail.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageBox"
-], function(Controller) {
+	"sap/m/MessageBox",
+	"sap/ui/core/routing/History"
+], function(Controller, History) {
 	"use strict";
 
 	return Controller.extend("EndersApp.controller.matDetail", {
@@ -9,13 +10,9 @@ sap.ui.define([
 		onInit: function() {
 		},
 		
-		goBack: function(oEvent) {
-			var oMainView = sap.ui.getCore().byId("__xmlview0");
-			var oSplitContainer = oMainView.byId("mainView");
-			oSplitContainer.setMode(sap.m.SplitAppMode.StretchCompressMode);
-			sap.ui.getCore().byId("__component0---Master").byId("masterView").setBusy(false);
+		goBack: function() {
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-			oRouter.navTo("mainDetail");
+			oRouter.navTo("detCreateOrder", true);
 
 		}
 
